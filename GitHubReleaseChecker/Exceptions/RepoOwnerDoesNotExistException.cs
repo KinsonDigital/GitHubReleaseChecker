fef1dcb242fa3dfdfab6c0b2b92772d740bc24a5
Calvin Wilkinson <kinsondigital@gmail.com>
2022-07-15 13:14:32 +0100

    Create exceptions for non-existing repo owners and repo names

---
+++ GitHubReleaseChecker/Exceptions/RepoOwnerDoesNotExistException.cs
@@ -0,0 +1,40 @@
+﻿// <copyright file="RepoOwnerDoesNotExistException.cs" company="KinsonDigital">
+// Copyright (c) KinsonDigital. All rights reserved.
+// </copyright>
+
+namespace GitHubReleaseChecker.Exceptions;
+
+/// <summary>
+/// Occurs when a string is null or empty.
+/// </summary>
+public class RepoOwnerDoesNotExistException : Exception
+{
+    /// <summary>
+    /// Initializes a new instance of the <see cref="RepoOwnerDoesNotExistException"/> class.
+    /// </summary>
+    public RepoOwnerDoesNotExistException()
+        : base("The repository owner does not exist.")
+    {
+    }
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="RepoOwnerDoesNotExistException"/> class.
+    /// </summary>
+    /// <param name="message">The message that describes the error.</param>
+    public RepoOwnerDoesNotExistException(string message)
+        : base(message)
+    {
+    }
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="RepoOwnerDoesNotExistException"/> class.
+    /// </summary>
+    /// <param name="message">The message that describes the error.</param>
+    /// <param name="innerException">
+    ///     The <see cref="Exception"/> instance that caused the current exception.
+    /// </param>
+    public RepoOwnerDoesNotExistException(string message, Exception innerException)
+        : base(message, innerException)
+    {
+    }
+}
---
+++ Testing/GitHubReleaseCheckerTests/Exceptions/RepoOwnerDoesNotExistExceptionTests.cs
@@ -0,0 +1,50 @@
+﻿// <copyright file="RepoOwnerDoesNotExistExceptionTests.cs" company="KinsonDigital">
+// Copyright (c) KinsonDigital. All rights reserved.
+// </copyright>
+
+using FluentAssertions;
+using GitHubReleaseChecker.Exceptions;
+
+namespace GitHubReleaseCheckerTests.Exceptions;
+
+/// <summary>
+/// Tests the <see cref="RepoOwnerDoesNotExistException"/> class.
+/// </summary>
+public class RepoOwnerDoesNotExistExceptionTests
+{
+    #region Constructor Tests
+    [Fact]
+    public void Ctor_WithNoParam_CorrectlySetsExceptionMessage()
+    {
+        // Act
+        var exception = new RepoOwnerDoesNotExistException();
+
+        // Assert
+        exception.Message.Should().Be("The repository owner does not exist.");
+    }
+
+    [Fact]
+    public void Ctor_WhenInvokedWithSingleMessageParam_CorrectlySetsMessage()
+    {
+        // Act
+        var exception = new RepoOwnerDoesNotExistException("test-message");
+
+        // Assert
+        exception.Message.Should().Be("test-message");
+    }
+
+    [Fact]
+    public void Ctor_WhenInvokedWithMessageAndInnerException_ThrowsException()
+    {
+        // Arrange
+        var innerException = new Exception("inner-exception");
+
+        // Act
+        var deviceException = new RepoOwnerDoesNotExistException("test-exception", innerException);
+
+        // Assert
+        deviceException.InnerException.Message.Should().Be("inner-exception");
+        deviceException.Message.Should().Be("test-exception");
+    }
+    #endregion
+}
