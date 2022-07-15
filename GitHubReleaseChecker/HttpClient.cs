5785432dc42b3d029e3b71a5d476c4fd7dc5e945
Calvin Wilkinson <kinsondigital@gmail.com>
2022-07-15 10:10:08 +0100

    Create HttpClient wrapper to enable testing

---
+++ GitHubReleaseChecker/HttpClient.cs
@@ -0,0 +1,81 @@
+﻿// <copyright file="HttpClient.cs" company="KinsonDigital">
+// Copyright (c) KinsonDigital. All rights reserved.
+// </copyright>
+
+using System.Diagnostics.CodeAnalysis;
+using System.Net;
+using GitHubReleaseChecker.Exceptions;
+using RestSharp;
+
+namespace GitHubReleaseChecker;
+
+/// <inheritdoc/>
+[ExcludeFromCodeCoverage]
+public sealed class HttpClient : IHttpClient
+{
+    private readonly RestClient restClient;
+    private string baseUrl = string.Empty;
+    private bool isDisposed;
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="HttpClient"/> class.
+    /// </summary>
+    public HttpClient() => this.restClient = new RestClient();
+
+    /// <inheritdoc/>
+    public string BaseUrl
+    {
+        get => this.baseUrl;
+        set
+        {
+            if (string.IsNullOrEmpty(value))
+            {
+                throw new NullOrEmptyStringException($"The property '{this.baseUrl}' value cannot be null or empty.");
+            }
+
+            while (value.EndsWith('/'))
+            {
+                value = value.TrimEnd('/');
+            }
+
+            this.baseUrl = value;
+        }
+    }
+
+    /// <inheritdoc/>
+    public async Task<(HttpStatusCode statusCode, T? data)> Get<T>(string requestUri)
+        where T : class
+    {
+        if (string.IsNullOrEmpty(requestUri))
+        {
+            throw new NullOrEmptyStringException($"The argument '{requestUri}' cannot be null or empty.");
+        }
+
+        while (requestUri.StartsWith('/'))
+        {
+            requestUri = requestUri.TrimStart('/');
+        }
+
+        var fullUrl = $"{BaseUrl}/{requestUri}";
+
+        this.restClient.AcceptedContentTypes = new[] { "application/vnd.github.v3+json" };
+        var request = new RestRequest(fullUrl);
+
+        var response = await this.restClient.ExecuteAsync<T>(request, Method.Get);
+
+        return (response.StatusCode, response.Data);
+    }
+
+    /// <inheritdoc/>
+    public void Dispose()
+    {
+        if (this.isDisposed)
+        {
+            return;
+        }
+
+        this.restClient.Dispose();
+
+        this.isDisposed = true;
+    }
+}
---
+++ GitHubReleaseChecker/IHttpClient.cs
@@ -0,0 +1,30 @@
+﻿using System.Net;
+
+namespace GitHubReleaseChecker;
+
+/// <summary>
+/// Provides a class for sending HTTP requests and receiving HTTP responses from a resource identified by a URI.
+/// </summary>
+public interface IHttpClient : IDisposable
+{
+    /// <summary>
+    /// Gets or sets the BaseUrl property for requests made by this client instance
+    /// </summary>
+    string BaseUrl { get; set; }
+
+    /// <summary>
+    /// Execute the request using GET HTTP method. Exception will be thrown if the request does not succeed.
+    /// The response data is deserialized to the Data property of the returned response object.
+    /// </summary>
+    /// <param name="requestUri">The URI that the request is sent to.</param>
+    /// <typeparam name="T">Target deserialization type.</typeparam>
+    /// <returns>
+    /// The following tuple:
+    /// <list type="bullet">
+    ///     <item><c>HttpStatusCode:</c> The HTTP status code of the request.</item>
+    ///     <item><c>T?:</c> The data returned by the request.</item>
+    /// </list>
+    /// </returns>
+    Task<(HttpStatusCode statusCode, T? data)> Get<T>(string requestUri)
+        where T : class;
+}
