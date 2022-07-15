dd0ae4dd3c446c4b0b838d9e22334c3e99518687
Calvin Wilkinson <kinsondigital@gmail.com>
2022-07-15 10:10:36 +0100

    Create models to hold data returned from GitHub API

---
+++ GitHubReleaseChecker/Models/OwnerInfoModel.cs
@@ -0,0 +1,17 @@
+﻿// <copyright file="OwnerInfoModel.cs" company="KinsonDigital">
+// Copyright (c) KinsonDigital. All rights reserved.
+// </copyright>
+
+// ReSharper disable AutoPropertyCanBeMadeGetOnly.Global
+namespace GitHubReleaseChecker.Models;
+
+/// <summary>
+/// Holds data about a repository owner.
+/// </summary>
+public record OwnerInfoModel
+{
+    /// <summary>
+    /// Gets or sets the login name of the owner.
+    /// </summary>
+    public string Login { get; set; } = string.Empty;
+}
---
+++ GitHubReleaseChecker/Models/ReleaseModel.cs
@@ -0,0 +1,23 @@
+﻿namespace GitHubReleaseChecker.Models;
+
+public record ReleaseModel
+{
+    public string HTMLURL { get; set; }
+    public string TagName { get; set; }
+    public string Name { get; set; }
+    public bool Draft { get; set; }
+    public bool PreRelease { get; set; }
+}
+
+
+/*
+ TODO: Delete this when finished.  This is the required JSON data from the GitHub API
+
+{
+    "html_url": "https://github.com/KinsonDigital/Velaptor/releases/tag/v1.0.0-preview.9",
+    "tag_name": "v1.0.0-preview.9",
+    "name": "v1.0.0-preview.9",
+    "draft": false,
+    "prerelease": true,
+}
+ */
